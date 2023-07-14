c4bee6314062d35e93318c846fc3392eb82c284e
Joswald Martinez <36779994+jdjoswald@users.noreply.github.com>
2023-07-14 23:14:05 +0200

    new imagen

--- ZooBitatApi/Models/Log.cs
+++ ZooBitatApi/Models/Log.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZooBitatApi.Models
 {
@@ -11,5 +12,11 @@ namespace ZooBitatApi.Models
         public string Message { get; set; }
         public string Level { get; set; }
         public string Section { get; set; }
+        /*public int IdUsuario { get; set; }
+        [ForeignKey("IdUsuario")]
+        public Usuario? Usuario { get; set; }*/
+
+
+
     }
 }
